283da1cd91f0c5b09f68b4726055998c62db720b
Vu Dong <vudongthien213@gmail.com>
2025-12-29 14:19:30 +0700

    Test deomo ArgoCD final

--- hello-k8s.py
+++ hello-k8s.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello k8s 3.0! Test demo flow Argo CD. Final version."
+    return "Hello k8s 3.0! Test demo ArgoCD Flow"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
