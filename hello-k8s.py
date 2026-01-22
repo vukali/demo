fb0ed8aa50a7b021ae34818a1cea359a8812ef84
Vu Dong <vudongthien213@gmail.com>
2026-01-22 16:50:13 +0700

    demo workflow push image to harbor with argocd

--- hello-k8s.py
+++ hello-k8s.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello k8s 3.0! Test demo ArgoCD Flow"
+    return "Welcome to Demo app! Test demo Harbor workflow. CI/CD"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
