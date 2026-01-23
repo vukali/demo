cbeb7ecc23a4d20a0b214c31b11735199f5e5fb9
Vu Dong <vudongthien213@gmail.com>
2026-01-23 12:13:04 +0700

    test downtime rolling docker swarm

--- hello-k8s.py
+++ hello-k8s.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Welcome to Demo app! Test demo Harbor workflow. CI/CD"
+    return "Welcome to Demo app! Test demo Harbor workflow. CI/CD, test down time"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
